5a3fd0f147e1f67ae3c44ae474874244a6b43d40
Zzathy <izzafathony27@gmail.com>
2026-06-09 10:09:27 +0700

    chore(deps): install Filament v5 core and admin panel provider

---
+++ app/Http/Controllers/CashFlowController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCashFlowRequest;
+use App\Http\Requests\UpdateCashFlowRequest;
+use App\Models\CashFlow;
+
+class CashFlowController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCashFlowRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CashFlow $cashFlow)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CashFlow $cashFlow)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCashFlowRequest $request, CashFlow $cashFlow)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CashFlow $cashFlow)
+    {
+        //
+    }
+}
